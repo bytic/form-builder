c57b19ccb294b377ce030a6e55b5589c9454e70c
Gabriel Solomon <hello@gabrielsolomon.ro>
2017-08-03 21:53:23 +0300

    initial add tests

---
+++ tests/fixtures/Application/Models/Fields/FormField.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ByTIC\FormBuilder\Tests\Fixtures\Application\Models\Fields;
+
+use ByTIC\Common\Records\Record;
+use ByTIC\FormBuilder\Application\Models\Fields\Traits\FormFieldTrait;
+use ByTIC\FormBuilder\Application\Models\Fields\Types\Traits\AbstractTypeTrait;
+
+/**
+ * Class FormField
+ * @package ByTIC\FormBuilder\Tests\Fixtures\Application\Modules\AbstractModule\Forms
+ */
+class FormField extends Record
+{
+    use FormFieldTrait;
+
+    /**
+     * @return AbstractTypeTrait
+     */
+    public function getType()
+    {
+        return 'text';
+    }
+}
---
+++ tests/src/Application/Modules/AbstractModule/Forms/DynamicFormTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ByTIC\FormBuilder\Tests\Application\Modules\AbstractModule\Forms;
+
+use ByTIC\FormBuilder\Tests\AbstractTest;
+use ByTIC\FormBuilder\Tests\Fixtures\Application\Modules\AbstractModule\Forms\DynamicForm;
+use Nip\Records\Collections\Collection;
+
+/**
+ * Class DynamicFormTest
+ * @package ByTIC\FormBuilder\Tests\Application\Modules\AbstractModule\Forms
+ */
+class DynamicFormTest extends AbstractTest
+{
+
+    public function testGetFields()
+    {
+        $form = new DynamicForm();
+        $fields = $form->getFields();
+
+        self::assertInstanceOf(Collection::class, $fields);
+
+    }
+}
