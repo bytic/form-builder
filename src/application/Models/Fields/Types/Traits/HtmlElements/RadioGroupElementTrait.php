623cae2d986885ec2d3e7dfd21d0d271d9c5c2f8
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-06-18 11:25:42 +0300

    add RadioGroupElement

---
+++ src/application/Models/Fields/Types/Traits/HtmlElements/RadioGroupElementTrait.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace ByTIC\FormBuilder\Application\Models\Fields\Types\Traits\HtmlElements;
+
+use ByTIC\FormBuilder\Application\Models\Fields\Types\Traits\AbstractTypeInterfaceTrait;
+use Nip_Form_Element_Abstract;
+use Nip_Form_Element_RadioGroup;
+use Nip_Form_Model as NipModelForm;
+
+/**
+ * Trait RadioGroupElementTrait
+ * @package ByTIC\FormBuilder\Application\Models\Fields\Types\Traits\HtmlElements
+ */
+trait RadioGroupElementTrait
+{
+    use AbstractTypeInterfaceTrait;
+
+    /**
+     * SelectElement constructor.
+     */
+    public function __construct()
+    {
+        $this->setInputType('radioGroup');
+    }
+
+    /**
+     * @param Nip_Form_Element_Abstract|Nip_Form_Element_RadioGroup $input
+     *
+     * @return mixed
+     */
+    public function initFormInput($input)
+    {
+        $this->populateFormInputOptions($input);
+
+        $input->getRenderer()->setSeparator('');
+
+        return parent::initFormInput($input);
+    }
+
+    /**
+     * @param Nip_Form_Element_Abstract|Nip_Form_Element_RadioGroup $input
+     */
+    public function populateFormInputOptions($input)
+    {
+        $values = $this->getItem()->getOption('check_options');
+        if (is_array($values)) {
+            foreach ($values as $value) {
+                $input->addOption($value, $value);
+            }
+        }
+    }
+
+    /**
+     * @var $form NipModelForm
+     */
+    public function adminGetDataFromModel($form)
+    {
+        parent::adminGetDataFromModel($form);
+
+        $form->addTextarea('check_options', translator()->translate('check_options'), true);
+        $form->getElement('check_options')->setValue(
+            implode("\n", $form->getModel()->getOption('check_options'))
+        );
+    }
+
+
+    /**
+     * @var $form NipModelForm
+     */
+    public function adminSaveToModel($form)
+    {
+        parent::adminSaveToModel($form);
+
+        $values = $form->getElement('check_options')->getValue();
+        $values = array_map('trim', explode("\n", $values));
+        $form->getModel()->setOption('check_options', $values);
+    }
+}
