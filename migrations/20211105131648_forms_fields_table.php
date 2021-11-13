523a7a47d03083e62ca5391ab8218dee322965d0
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-11-13 21:01:12 +0200

    initial update fields database definition

--- migrations/20211105131648_forms_fields_table.php
+++ migrations/20211105131648_forms_fields_table.php
@@ -25,5 +25,37 @@ final class FormsFieldsTable extends AbstractMigration
             return;
         }
 
+        $table = $this->table($table_name);
+        $table
+            ->addColumn('id_form', 'integer')
+            ->addColumn('type', 'string')
+            ->addColumn('role', 'string')
+            ->addColumn('label', 'string', ['length' => 500])
+            ->addColumn('label_intern', 'string')
+            ->addColumn('help', 'text')
+            ->addColumn('options', 'text')
+            ->addColumn('visible', 'enum', ['values' => ['yes', 'no', 'hidden'], 'default' => 'yes'])
+            ->addColumn('listing', 'set', ['values' => ['public', 'admin']])
+            ->addColumn('filter', 'set', ['values' => ['public', 'admin']])
+            ->addColumn('mandatory', 'enum', ['values' => ['yes', 'no'], 'default' => 'no'])
+            ->addColumn('pos', 'integer', ['limit' => \Phinx\Db\Adapter\MysqlAdapter::INT_TINY])
+            ->addColumn('modified', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP',
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+            ]);
+
+        $table->addIndex(['id_form']);
+        $table->addIndex(['type']);
+
+        $table->save();
+
+        $table->addForeignKey('id_form',
+            FormsBuilderModels::forms()->getFullNameTable(),
+            'id',
+            ['constraint' => 'forms_id_forms_values_id_form', 'delete' => 'RESTRICT', 'update' => 'NO_ACTION'])
+            ->save();
     }
 }
