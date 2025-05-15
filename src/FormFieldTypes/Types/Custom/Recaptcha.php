d88e9849f287fefac67a4f3f976519ec83f56d76
Gabriel Solomon <hello@gabrielsolomon.ro>
2025-05-15 11:32:54 +0300

    feat: add recaptcha element

--- src/FormFieldTypes/Types/Custom/Recaptcha.php
+++ src/FormFieldTypes/Types/Custom/Recaptcha.php
@@ -3,17 +3,33 @@
 namespace ByTIC\FormBuilder\FormFieldTypes\Types\Custom;
 
 
+use ByTIC\FormBuilder\FormFieldTypes\Icons\FieldIcons;
 use ByTIC\FormBuilder\FormFieldTypes\Types\Behaviours\AbstractTypeInterfaceTrait;
 use ByTIC\FormBuilder\FormFieldTypes\Types\Behaviours\IsUnique\IsUniqueTrait;
+use ByTIC\FormBuilder\FormFieldTypes\Types\Behaviours\Mandatory\IsDefaultMandatoryTrait;
 
 class Recaptcha extends AbstractType
 {
     use AbstractTypeInterfaceTrait;
     use IsUniqueTrait;
+    use IsDefaultMandatoryTrait;
 
     public function __construct()
     {
         parent::__construct();
         $this->setInputType('recaptcha');
+    }
+
+    /**
+     * @return string
+     */
+    protected function getDefaultIcon(): string
+    {
+        return FieldIcons::SHIELD;
+    }
+
+    public function generateFormName() : string
+    {
+        return \Nip_Form_Element_Recaptcha::FORM_NAME;
     }
 }
